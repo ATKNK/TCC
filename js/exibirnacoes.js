16d3e6d662a0fda5454e8a9b2c1151f778c9503e
ArthurNakayama <88379803+ArthurNakayama@users.noreply.github.com>
2023-10-10 09:41:09 -0300

    Sétimo Commit

--- js/exibirnacoes.js
+++ js/exibirnacoes.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const trocarAlemanha = document.getElementById("trocarAlemanha");
     const imgAlemanha = "flags/ALEMANHA1914.png";
-
+    const alemanha = document.getElementById("ALEMANHA")
 
     const trocarReinoUnido = document.getElementById("trocarReinoUnido");
     const imgReinoUnido = "flags/REINOUNIDO.png";
@@ -15,29 +15,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const trocarAlemanha1920 = document.getElementById("trocarAlemanha1920");
     const imgAlemanha1920 = "flags/ALEMANHA1920.png";
 
+    function teste() {
+        console.log("TESTE FUNCIONA")
+    }
 
-
-    trocarAlemanha.addEventListener("click", function() {
+    function Alemanha1914() {
         bandeira.src = imgAlemanha;
         console.log("ALEMANHA TROCADA");
         Nome_Pais.innerHTML = "Império Alemão";
         Governo_Pais.innerHTML = "Monarquia Parlamentarista Federal";
         Capital_Pais.innerHTML = "Berlim";
-    });
+    };
 
-    trocarReinoUnido.addEventListener("click", function() {
+    function ReinoUnido() {
         bandeira.src = imgReinoUnido;
         console.log("REINO UNIDO TROCADO");
         Nome_Pais.innerHTML = "Reino Unido";
         Governo_Pais.innerHTML = "Monarquia Parlamentarista";
         Capital_Pais.innerHTML = "Londres"
-    });
+    };
 
-    trocarAlemanha1920.addEventListener("click", function() {
+    function Alemanha1920() {
         bandeira.src = imgAlemanha1920;
         console.log("WEIMAR TROCADA");
         Nome_Pais.innerHTML = "República de Weimar";
         Governo_Pais.innerHTML = "República";
         Capital_Pais.innerHTML = "Berlim";
-    });
+    };
+
+    trocarAlemanha.addEventListener("click", Alemanha1914);
+    trocarReinoUnido.addEventListener("click", ReinoUnido);
+    trocarAlemanha1920.addEventListener("click", Alemanha1920);
 });
