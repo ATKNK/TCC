c3c95724d8ef759b058e6f9a152485b584106938
Arthur Nakayama <61486792+ATKNK@users.noreply.github.com>
2023-10-11 16:51:08 -0300

    Feito Mapa Chamar funcao Alemanha1914()
    Falta só arrumar uma forma de trocar as imagens

--- js/exibirnacoes.js
+++ js/exibirnacoes.js
@@ -1,31 +1,61 @@
+var bandeira = document.getElementById("bandeira");
+    var Nome_Pais = document.getElementById("Nome_Pais");
+    var Governo_Pais = document.getElementById("Governo_Pais");
+    var Capital_Pais = document.getElementById("Capital_Pais");
+
+    var trocarAlemanha = document.getElementById("trocarAlemanha");
+    var imgAlemanha = "flags/ALEMANHA1914.png";
+    var alemanha = document.getElementById("ALEMANHA")
+
+    var trocarReinoUnido = document.getElementById("trocarReinoUnido");
+    var imgReinoUnido = "flags/REINOUNIDO.png";
+
+    var trocarAlemanha1920 = document.getElementById("trocarAlemanha1920");
+    var imgAlemanha1920 = "flags/ALEMANHA1920.png";
+
+
+function Alemanha1914() {
+   // bandeira.src = imgAlemanha;
+    console.log("ALEMANHA TROCADA");
+    Nome_Pais.innerHTML = "Império Alemão";
+    Governo_Pais.innerHTML = "Monarquia Parlamentarista Federal";
+    Capital_Pais.innerHTML = "Berlim";
+};
+
+function alemanhafuncao(){
+    alert("Clicou aqui ?")
+
+    return Alemanha1914();
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 
-    const bandeira = document.getElementById("bandeira");
-    const Nome_Pais = document.getElementById("Nome_Pais");
-    const Governo_Pais = document.getElementById("Governo_Pais");
-    const Capital_Pais = document.getElementById("Capital_Pais");
+    var bandeira = document.getElementById("bandeira");
+    var Nome_Pais = document.getElementById("Nome_Pais");
+    var Governo_Pais = document.getElementById("Governo_Pais");
+    var Capital_Pais = document.getElementById("Capital_Pais");
 
-    const trocarAlemanha = document.getElementById("trocarAlemanha");
-    const imgAlemanha = "flags/ALEMANHA1914.png";
-    const alemanha = document.getElementById("ALEMANHA")
+    var trocarAlemanha = document.getElementById("trocarAlemanha");
+    var imgAlemanha = "flags/ALEMANHA1914.png";
+    var alemanha = document.getElementById("ALEMANHA")
 
-    const trocarReinoUnido = document.getElementById("trocarReinoUnido");
-    const imgReinoUnido = "flags/REINOUNIDO.png";
+    var trocarReinoUnido = document.getElementById("trocarReinoUnido");
+    var imgReinoUnido = "flags/REINOUNIDO.png";
 
-    const trocarAlemanha1920 = document.getElementById("trocarAlemanha1920");
-    const imgAlemanha1920 = "flags/ALEMANHA1920.png";
+    var trocarAlemanha1920 = document.getElementById("trocarAlemanha1920");
+    var imgAlemanha1920 = "flags/ALEMANHA1920.png";
 
     function teste() {
         console.log("TESTE FUNCIONA")
     }
 
-    function Alemanha1914() {
-        bandeira.src = imgAlemanha;
-        console.log("ALEMANHA TROCADA");
-        Nome_Pais.innerHTML = "Império Alemão";
-        Governo_Pais.innerHTML = "Monarquia Parlamentarista Federal";
-        Capital_Pais.innerHTML = "Berlim";
-    };
+    // function Alemanha1914() {
+    //     bandeira.src = imgAlemanha;
+    //     console.log("ALEMANHA TROCADA");
+    //     Nome_Pais.innerHTML = "Império Alemão";
+    //     Governo_Pais.innerHTML = "Monarquia Parlamentarista Federal";
+    //     Capital_Pais.innerHTML = "Berlim";
+    // };
 
     function ReinoUnido() {
         bandeira.src = imgReinoUnido;
