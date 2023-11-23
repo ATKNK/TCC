b202bf9f6b7105e72135085efdb5d622959849b5
ArthurNakayama <88379803+ArthurNakayama@users.noreply.github.com>
2023-11-23 10:47:39 -0300

    Mapa de 1945 adicionado

--- testes do tcc/js/alternarmapas.js
+++ testes do tcc/js/alternarmapas.js
@@ -1,37 +1,64 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var SliderTimeline = document.getElementById("SliderTimeline");
+    var AnoSlider = document.getElementById("AnoSlider");
+    AnoSlider.innerHTML = SliderTimeline.value;
+    
+    const ano = document.getElementById("Ano");
     const mapa1914 = document.getElementById("mapa1914");
-    const botao1914 = document.getElementById("botao1914");
     const mapa1920 = document.getElementById("mapa1920");
-    const botao1920 = document.getElementById("botao1920");
     const mapa1936 = document.getElementById("mapa1936");
-    const botao1936 = document.getElementById("botao1936");
+    const mapa1939 = document.getElementById("mapa1939");
+    const mapa1945 = document.getElementById("mapa1945");
 
-    var ano = 1914;
+    SliderTimeline.oninput = function(){
+        var testevalor = this.value;
+        console.log(testevalor);
+        if(testevalor==1){
+            AnoSlider.innerHTML = "1914";
 
-    botao1914.addEventListener("click", function() {
-        mapa1914.style.display = "block";
-        mapa1920.style.display = "none";
-        mapa1936.style.display = "none";
-        ano = 1914;
-        const elementoAno = document.getElementById("Ano");
-        elementoAno.textContent = ano;
-    });
+                mapa1914.style.display = "block";
+                mapa1920.style.display = "none";
+                mapa1936.style.display = "none";
+                mapa1939.style.display = "none";
+                mapa1945.style.display = "none";
+                ano.innerHTML = "1914"
+        }
+        else if(testevalor==2){
+            AnoSlider.innerHTML = "1920";
 
-    botao1920.addEventListener("click", function() {
-        mapa1914.style.display = "none";
-        mapa1920.style.display = "block";
-        mapa1936.style.display = "none";
-        ano = 1920;
-        const elementoAno = document.getElementById("Ano");
-        elementoAno.textContent = ano;
-    });
-
-    botao1936.addEventListener("click", function() {
-        mapa1914.style.display = "none";
-        mapa1920.style.display = "none";
-        mapa1936.style.display = "block";
-        ano = 1936;
-        const elementoAno = document.getElementById("Ano");
-        elementoAno.textContent = ano;
-    });
+                mapa1914.style.display = "none";
+                mapa1920.style.display = "block";
+                mapa1936.style.display = "none";
+                mapa1939.style.display = "none";
+                mapa1945.style.display = "none";
+                ano.innerHTML = "1920"
+        }
+        else if(testevalor==3){
+            AnoSlider.innerHTML = "1936";
+                mapa1914.style.display = "none";
+                mapa1920.style.display = "none";
+                mapa1936.style.display = "block";
+                mapa1939.style.display = "none";
+                mapa1945.style.display = "none";
+                ano.innerHTML = "1936"
+        }
+        else if(testevalor==4){
+            AnoSlider.innerHTML = "1939";
+            mapa1914.style.display = "none";
+            mapa1920.style.display = "none";
+            mapa1936.style.display = "none";
+            mapa1939.style.display = "block";
+            mapa1945.style.display = "none";
+            ano.innerHTML = "1939"
+        }
+        else if(testevalor==5){
+            AnoSlider.innerHTML = "1945";
+            mapa1914.style.display = "none";
+            mapa1920.style.display = "none";
+            mapa1936.style.display = "none";
+            mapa1939.style.display = "none";
+            mapa1945.style.display = "block";
+            ano.innerHTML = "1945"
+        }
+    }
 });
